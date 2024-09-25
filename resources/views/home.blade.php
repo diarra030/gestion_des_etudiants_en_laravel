70db8d695060ac40ced360a637aeeafe70a88857
Diarra <diarraaboubacar030@gmail.com>
2024-09-25 11:20:58 +0000

    modification de la page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,6 +44,13 @@
           </div>
         </div>
       </nav>
+
+
+      <div class="container" style="display:flex; justify-content:center; align-item-center">
+        <h1>
+          BIENVENUE SUR LA PLATEFORME DE GESTION DES ETUDIANTS
+        </h1>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
