d54c44421228ac7b85e24fbce5c4ba6656f5e743
Diarra <diarraaboubacar030@gmail.com>
2024-09-25 16:30:27 +0000

    j ai modifier le C de mon modelClient

--- app/Models/client.php
+++ app/Models/client.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class client extends Authenticatable
+class Client extends Authenticatable
 {
     use HasFactory;
 
